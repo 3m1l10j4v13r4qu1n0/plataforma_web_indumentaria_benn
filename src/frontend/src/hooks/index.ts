83c45e84b03c51b0261477aefe4392d8526dfbb0
Emilio Javier Aquino <aquinoemiliojavier@gmail.com>
2026-06-22 11:51:07 +0000

    chore(hooks): actualizar exportaciones con hooks de HU-01

--- src/frontend/src/hooks/index.ts
+++ src/frontend/src/hooks/index.ts
@@ -1,2 +1,9 @@
+// HU-06
 export { useBuscarProductos, productosQueryKeys } from './useBuscarProductos';
 export { useStockSearch } from './useStockSearch';
+
+// HU-01
+export { useProductSearch, productSearchQueryKeys } from './useProductSearch';
+export { useCart } from './useCart';
+export { useCartValidation } from './useCartValidation';
+export { useProcessSale } from './useProcessSale';
