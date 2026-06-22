c828ec145e1b539836467b0963f007b2d891234b
Emilio Javier Aquino <aquinoemiliojavier@gmail.com>
2026-06-22 11:11:02 +0000

    chore(components): actualizar exportaciones con componentes de HU-01

--- src/frontend/src/components/ui/index.ts
+++ src/frontend/src/components/ui/index.ts
@@ -1,5 +1,15 @@
+// HU-06
 export { StockBadge, type StockLevel } from './StockBadge';
 export { StockSearchInput } from './StockSearchInput';
 export { ResultsHeader } from './ResultsHeader';
 export { StockResultCard, type StockResultCardProps } from './StockResultCard';
 export { EmptyState } from './EmptyState';
+
+// HU-01
+export { StockIndicator, type StockStatus } from './StockIndicator';
+export { QuantityInput } from './QuantityInput';
+export { CartItemRow, type CartItemRowProps } from './CartItemRow';
+export { CartTable } from './CartTable';
+export { StockAlert, type StockAlertItem } from './StockAlert';
+export { SaleSummary } from './SaleSummary';
+export { SaleActionBar } from './SaleActionBar';
