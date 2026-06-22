28a6a49787d475afffe433b90cc5374ae6afcdd8
Emilio Javier Aquino <aquinoemiliojavier@gmail.com>
2026-06-22 14:56:30 +0000

    chore(hooks): actualizar exportaciones con usePrinter de HU-07

--- src/frontend/src/hooks/index.ts
+++ src/frontend/src/hooks/index.ts
@@ -7,3 +7,6 @@ export { useProductSearch, productSearchQueryKeys } from './useProductSearch';
 export { useCart } from './useCart';
 export { useCartValidation } from './useCartValidation';
 export { useProcessSale } from './useProcessSale';
+
+// HU-07
+export { usePrinter } from './usePrinter';
