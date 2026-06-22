9ddbeddb3bba5a1bb4538988c233620466a2dc59
emiliojavieraquino <aquinoemiliojavier@gmail.com>
2026-06-21 22:27:14 -0300

    chore(components): centralizar exportaciones de UI y layout

---
+++ src/frontend/src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { StockBadge, type StockLevel } from './StockBadge';
+export { StockSearchInput } from './StockSearchInput';
+export { ResultsHeader } from './ResultsHeader';
+export { StockResultCard, type StockResultCardProps } from './StockResultCard';
+export { EmptyState } from './EmptyState';
