3861dbdb4231f3d522ed32f18fda84d4a3cd60ba
Emilio Javier Aquino <aquinoemiliojavier@gmail.com>
2026-06-30 16:53:44 +0000

    fix(frontend): Error de importcacion en archivo main.tsx

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './contexts/AuthContext';
-import { AppRouter } from './routes/AppRouter';
+import { AuthProvider } from './contexts/AuthContext.tsx';
+import { AppRouter } from './routes/AppRouter.tsx';
 import './index.css';
 
 const queryClient = new QueryClient({
